8c6a512a2a1447041e5b00cc07e2d19d212c371a
kabix09 <kabix.009@gmail.com>
2026-03-07 08:49:47 +0100

    feat: add event store

---
+++ src/Infrastructure/Persistence/Doctrine/Entity/EventStoreEntry.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'event_store')]
+class EventStoreEntry
+{
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $eventName;
+
+    #[ORM\Column(type: 'string', length: 36)]
+    private string $aggregateId;
+
+    #[ORM\Column(type: 'json')]
+    private array $payload;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $occurredAt;
+
+    public function __construct(string $eventName, string $aggregateId, array $payload, \DateTimeImmutable $occurredAt)
+    {
+        $this->eventName = $eventName;
+        $this->aggregateId = $aggregateId;
+        $this->payload = $payload;
+        $this->occurredAt = $occurredAt;
+    }
+}
